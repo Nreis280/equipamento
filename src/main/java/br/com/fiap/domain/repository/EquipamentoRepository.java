e9973a79f628d7c6786da79a4c937c47c72c2b05
Nreis280 <nicolasreissanto@gmail.com>
2023-09-29 20:10:04 -0300

    Simulado Finalizado ultima versão

--- src/main/java/br/com/fiap/domain/repository/EquipamentoRepository.java
+++ src/main/java/br/com/fiap/domain/repository/EquipamentoRepository.java
@@ -13,156 +13,155 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class EquipamentoRepository implements Repository<Equipamento, Long> {
 
-    private static final AtomicReference<EquipamentoRepository> instance = new AtomicReference<>();
+    private static final AtomicReference<EquipamentoRepository> instance  = new AtomicReference<>();
 
-    private EquipamentoRepository() {
+    private EquipamentoRepository(){
+
     }
 
-
-    public static EquipamentoRepository build() {
+    public static EquipamentoRepository build(){
         EquipamentoRepository result = instance.get();
-        if (Objects.isNull(result)) {
+        if (Objects.isNull(result)){
             EquipamentoRepository repo = new EquipamentoRepository();
-            if (instance.compareAndSet(null, repo)) {
+            if (instance.compareAndSet(null,repo)){
                 result = repo;
-            } else {
+            }else {
                 result = instance.get();
             }
         }
         return result;
     }
 
-
-    @Override
-    public Equipamento persist(Equipamento equipamento) {
-        var sql = "BEGIN" +
-                "INSERT INTO cliente (NM_EQUIPAMENTO) " +
-                "VALUES (?) " +
-                "returning ID_EQUIPAMENTO into ?; " +
-                "END;" +
-                "";
+    public Equipamento persist(Equipamento equipamento){
+        var sql = "BEGIN" + " INSERT INTO equipamento (NM_EQUIPAMENTO,DS_EQUIPAMENTO)" +  "VALUES (?,?) "
+                + "returning ID_EQUIPAMENTO into ?; " + "END;" + "";
 
         var factory = ConnectionFactory.build();
         Connection connection = factory.getConnection();
 
-
         CallableStatement cs = null;
+
         try {
-            cs = connection.prepareCall(sql);
-            cs.setString(1, equipamento.getNome());
-            cs.registerOutParameter(2, Types.BIGINT);
+            cs = connection.prepareCall( sql );
+            cs.setString( 1, equipamento.getNome() );
+            cs.setString( 2, equipamento.getDescrição() );
+            cs.registerOutParameter( 3, Types.BIGINT );
             cs.executeUpdate();
-            equipamento.setId(cs.getLong(2));
+            equipamento.setId( cs.getLong( 3 ) );
             cs.close();
             connection.close();
-
         } catch (SQLException e) {
-            System.err.println("Não foi possível executar o comando!\n" + e.getMessage());
+            System.err.println( "Não foi possível executar o comando!\n" + e.getMessage() );
         }
         return equipamento;
     }
 
-    @Override
-    public List<Equipamento> findAll() {
+    public List<Equipamento> findAll(){
         List<Equipamento> equipamentos = new ArrayList<>();
 
         try {
-            var factory = ConnectionFactory.build();
+            var factory =   ConnectionFactory.build();
             Connection connection = factory.getConnection();
 
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM EQUIPAMENTO");
 
-            if (resultSet.isBeforeFirst()) {
-                while (resultSet.next()) {
+            if (resultSet.isBeforeFirst()){
+                while (resultSet.next()){
                     Long id = resultSet.getLong("ID_EQUIPAMENTO");
                     String nome = resultSet.getString("NM_EQUIPAMENTO");
-                    String descricao = resultSet.getString(" DS_EQUIPAMENTO");
-                    //Adicionando Equipamentos na coleção
-                    equipamentos.add(new Equipamento(id, nome, descricao));
+                    String descrição = resultSet.getString("DS_EQUIPAMENTO");
+
+                    //adicionando equipamento na coleção
+                    equipamentos.add(new Equipamento(id,nome, descrição));
                 }
             }
+
             resultSet.close();
             statement.close();
             connection.close();
-        } catch (SQLException e) {
-            System.err.println("Não foi possivel consultar os dados!\n" + e.getMessage());
+
+        }catch (SQLException e){
+            System.err.println("Não foi possivel consultar os dados!\n" + e.getMessage() );
         }
         return equipamentos;
     }
 
-    @Override
-    public Equipamento findById(Long id) {
-        Equipamento equipamento = null;
-        var sql = "SELECT * FROM cliente where ID_EQUIPAMENTO=?";
 
-        var factory = ConnectionFactory.build();
+    public Equipamento findById(Long id){
+        Equipamento equipamento = null;
+        var sql = "SELECT * FROM equipamento where ID_EQUIPAMENTO=?";
+
+        var factory =   ConnectionFactory.build();
         Connection connection = factory.getConnection();
 
         try {
-                PreparedStatement preparedStatement = connection.prepareStatement(sql);
-                preparedStatement.setLong(1, id);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                if (resultSet.isBeforeFirst()) {
-                    while (resultSet.next()) {
-                        equipamento = new Equipamento(
-                                resultSet.getLong("ID_EQUIPAMENTO"),
-                                resultSet.getString("NM_EQUIPAMENTO"),
-                                resultSet.getString(" DS_EQUIPAMENTO")
-                        );
-                    }
-                } else {
-                    System.out.println("Cliente não encontrado com o id = " + id);
+            PreparedStatement preparedStatement = connection.prepareStatement( sql );
+            preparedStatement.setLong( 1, id );
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.isBeforeFirst()) {
+                while (resultSet.next()) {
+                    equipamento = new Equipamento(
+                            resultSet.getLong( "ID_EQUIPAMENTO" ),
+                            resultSet.getString( "NM_EQUIPAMENTO" ),
+                            resultSet.getString("DS_EQUIPAMENTO")
+                    );
                 }
-                resultSet.close();
-                preparedStatement.close();
-                connection.close();
-            }catch (SQLException e) {
+            } else {
+                System.out.println( "Equipamento não encontrado com o id = " + id );
+            }
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
             System.err.println( "Não foi possível executar a consulta: \n" + e.getMessage() );
         }
         return equipamento;
     }
 
-        @Override
-        public Equipamento update (Equipamento equipamento){
-            PreparedStatement ps = null;
+    public Equipamento update (Equipamento equipamento){
+        PreparedStatement ps = null;
 
-            var sql = "UPDATE equipamento SET NM_EQUIPAMEnTO = ? where ID_EQUIPAMENTO";
-            var factory = ConnectionFactory.build();
-            Connection connection = factory.getConnection();
-            try {
-                ps = connection.prepareStatement(sql);
-                ps.setString(1, equipamento.getNome());
-                ps.setLong(2,equipamento.getId());
-                int intensAtualizados = ps.executeUpdate();
+        var sql = "UPDATE equipamento SET NM_EQUIPAMENTO = ? where ID_EQUIPAMENTO =?";
 
-                ps.close();
-                connection.close();
-                if (intensAtualizados > 0) return  findById(equipamento.getId());
-            }catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            return null;
+        var factory =   ConnectionFactory.build();
+        Connection connection = factory.getConnection();
+
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, equipamento.getNome());
+            ps.setLong(2, equipamento.getId());
+            int intensAtualizados = ps.executeUpdate();
+
+            ps.close();
+            connection.close();
+            if (intensAtualizados > 0) return findById((equipamento.getId()));
+        }catch (SQLException e){
+            throw  new RuntimeException(e);
         }
+        return null;
+    }
 
-        @Override
-        public boolean delete (Long id){
-            PreparedStatement ps = null;
+    public boolean delete(Long id){
+        PreparedStatement ps = null;
 
-            var sql = "DELETE from equipamento where ID_EQUIPAMENTO =?";
-            var factory = ConnectionFactory.build();
-            Connection connection = factory.getConnection();
-            try {
-                ps = connection.prepareStatement(sql);
-                ps.setLong(1, id);
-                int intensRemovidos = ps.executeUpdate();
-                ps.close();
-                connection.close();
-                if (intensRemovidos > 0) return true;
-            }catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            return false;
+        var sql = "DELETE FROM SET EQUIPAMENTO WHERE ID_EQUIPAMENTO =?";
+
+        var factory =   ConnectionFactory.build();
+        Connection connection = factory.getConnection();
+
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setLong(1,id);
+            int itensRemovidos = ps.executeUpdate();
+            ps.close();
+            connection.close();
+            if (itensRemovidos > 0) return true;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
         }
+        return false;
+    }
+
 }
 
--- src/main/java/br/com/fiap/domain/resource/EquipamentoResource.java
+++ src/main/java/br/com/fiap/domain/resource/EquipamentoResource.java
@@ -3,66 +3,72 @@ package br.com.fiap.domain.resource;
 import br.com.fiap.domain.entity.Equipamento;
 import br.com.fiap.domain.repository.EquipamentoRepository;
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.UriBuilder;
-import jakarta.ws.rs.core.UriInfo;
+import jakarta.ws.rs.core.*;
 
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+@Path("/equipamento")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+
+
 
 public class EquipamentoResource implements Resource<Equipamento, Long> {
 
+    @Context
     UriInfo uriInfo;
 
-    private EquipamentoRepository repo = EquipamentoRepository.build();
+    private EquipamentoRepository repo =  EquipamentoRepository.build();
+
+
+    @Override
+    @GET
+    public Response findAll() {
+        List<Equipamento> equipamentos = new ArrayList<>();
+        equipamentos = repo.findAll();
+        return Response.ok(equipamentos).build();
+    }
+
+    @Override
+    @GET
+    @Path("/{id}")
+    public Response findById(@PathParam("id") Long id) {
+        Equipamento equipamento = repo.findById(id);
+        if (Objects.isNull(equipamento)) return Response.status(404).build();
+        return Response.ok(equipamento).build();
+    }
+
+
     @POST
     @Override
-    public Response persist(Equipamento equipamento) {
-        Equipamento persist = repo.persist(equipamento);
+    public Response persist(Equipamento e) {
+        Equipamento equipamento = repo.persist(e);
         //Criando a URI da requisição
         UriBuilder ub = uriInfo.getAbsolutePathBuilder();
-        URI uri = ub.path(String.valueOf(persist.getId())).build();
+        URI uri = ub.path(String.valueOf(equipamento.getId())).build();
         return Response.created(uri).entity(equipamento).build();
     }
 
-    @GET
-    @Override
-    public Response findAll() {
-        List<Equipamento> all = repo.findAll();
-        return Response
-                .status(Response.Status.OK)
-                .entity(all)
-                .build();
-    }
 
-    @GET
-    @Path("/{id}")
-    @Override
-    public Response findById(@PathParam("id") Long id) {
-        Equipamento e = repo.findById(id);
-        if (Objects.isNull(e)) return Response.status(404).build();
-        return Response
-                .status(Response.Status.OK)
-                .entity(e)
-                .build();
-    }
     @PUT
     @Path("/{id}")
     @Override
-    public Response update(@PathParam("id") Long id, Equipamento equipamento) {
+    public Response update(@PathParam("id")Long id, Equipamento equipamento){
         equipamento.setId(id);
         Equipamento e = repo.update(equipamento);
         if (Objects.isNull(e)) return Response.status(404).build();
         return Response.ok().entity(e).build();
     }
+
+    @Override
     @DELETE
     @Path("/{id}")
-    @Override
-    public Response delete(@PathParam("id") Long id) {
+    public Response delete(@PathParam("id")Long id){
         boolean deleted = repo.delete(id);
         if (deleted) return Response.ok().build();
         return Response.status(404).build();
     }
+
 }
